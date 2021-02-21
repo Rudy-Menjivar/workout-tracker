2bd9f738490da45d2db1b44fcd3eced6e378ab73
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-02-20 23:02:31 -0800

    added html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,13 @@
+// Dependencies
+const path = require("path");
+const router = require("express").Router()
+
+// Routes
+router.get("/exercise",(req,res) => {
+    res.sendFile(path.join(__dirname,"../public/exercise.html"))
+})
+
+router.get("/stats",(req,res) => {
+    res.sendFile(path.join(__dirname,"../public/stats.html"))
+})
+module.exports = router
