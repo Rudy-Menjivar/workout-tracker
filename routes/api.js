6adebe3c5644712a9ca13f3826b43583359fe4f0
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-02-22 17:33:34 -0800

    added api routes

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,53 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+  .sort({ date: -1 })
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  router.put("/api/workouts/:id", (req, res) => {
+    Workout.updateOne({ _id: req.params.id }, { 
+      $push: {
+        exercises: req.body 
+      } 
+    },
+      // runValidators ensures new exercises will meet schema requirements
+      { 
+        new: true,
+        runValidators: true 
+      }
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
 module.exports = router;
